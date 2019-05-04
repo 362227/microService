ceeee553fd5333204c05a39886fd9747e97272c5
ccss <cristian.salazar@homeselect.com>
2019-05-04 09:47:06 -0500

    fixing model

--- app/Turno.php
+++ app/Turno.php
@@ -9,7 +9,7 @@ class Turno extends Model
     protected $table = 'turno';
 
     protected $fillable = ['documento_usuario','cod_tipo_documento','cod_consultorio','documento_medico','cod_tipo_documento_medico', 'cod_horario'];
-
+    protected $primaryKey = 'cod_turno';
     public $timestamps = false;
 
 }
